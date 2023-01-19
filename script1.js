b6e5def0441027379239ea16348e596c6f28df92
Roman Wurz <wurzroman@gmail.com>
2023-01-19 19:36:23 +0100

    changed everything

---
+++ script1.js
@@ -0,0 +1,8 @@
+function functionParagraph() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+
+function functionToggleDark() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
